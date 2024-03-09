3808e5ed1360106a55843c0aacc1199a9cb2ca14
Minmin Gong <gongminmin@msn.com>
2024-03-08 23:43:02 -0800

    [Circuit][Code] Add a DHT module to measure environment temperature and humidity

--- Code/Control/Control.ino
+++ Code/Control/Control.ino
@@ -2,11 +2,15 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <DHT.h>
+
 constexpr uint32_t DisplayAddr = 0x3C;
 
 constexpr int TemperatureSensorReadPin = A0;
 constexpr int SensorsPowerPin = 7;
 constexpr int CoolingPlatePin = 6;
+
+constexpr int DhtPin = 5;
 
 Adafruit_SSD1306& GetDisplay()
 {
@@ -17,6 +21,13 @@ Adafruit_SSD1306& GetDisplay()
   // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
   static Adafruit_SSD1306 display(ScreenWidth, ScreenHeight, &Wire, OledReset);
   return display;
+}
+
+DHT& GetDht()
+{
+  constexpr uint32_t DhtType = DHT11;
+  static DHT dht(DhtPin, DhtType);
+  return dht;
 }
 
 float CalcTemperature(int temperature_sensor)
@@ -75,6 +86,7 @@ bool TriggerState(float value, float lower_bound, float upper_bound, bool curr_s
 void setup()
 {
   Adafruit_SSD1306& display = GetDisplay();
+  DHT& dht = GetDht();
 
   Serial.begin(9600);
   if (!display.begin(SSD1306_SWITCHCAPVCC, DisplayAddr)) {
@@ -84,12 +96,16 @@ void setup()
     }
   }
 
+  dht.begin();
+
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
+  display.dim(true);
   display.display();
 
   pinMode(CoolingPlatePin, OUTPUT);
+  digitalWrite(CoolingPlatePin, LOW);
 
   pinMode(SensorsPowerPin, OUTPUT);
   digitalWrite(SensorsPowerPin, LOW);
@@ -98,6 +114,7 @@ void setup()
 void loop()
 {
   Adafruit_SSD1306& display = GetDisplay();
+  DHT& dht = GetDht();
 
   digitalWrite(SensorsPowerPin, HIGH);
   delay(10);
@@ -109,17 +126,19 @@ void loop()
   float temperature = CalcTemperature(temperature_sensor);
 
   display.clearDisplay();
+
   display.setCursor(0, 8);
+  display.print("Cold");
+  display.setCursor(0, 16);
   display.print(temperature);
   display.print(static_cast<char>(247));
   display.print("C");
-
-  display.setCursor(0, 40);
-  display.print(millis() / 1000.0f);
+  display.setCursor(0, 24);
+  display.print(millis() / 1000.0f, 1);
 
   constexpr float TemperatureLowerBound = 14;
   constexpr float TemperatureUpperBound = TemperatureLowerBound + 5;
-  static bool cooling_plate_on = true;
+  static bool cooling_plate_on = false;
   cooling_plate_on = TriggerState(temperature, TemperatureLowerBound, TemperatureUpperBound, cooling_plate_on); 
   digitalWrite(CoolingPlatePin, cooling_plate_on ? HIGH : LOW);
 
@@ -127,6 +146,20 @@ void loop()
   {
     display.setCursor(108, 32);
     display.print('C');
+  }
+
+  float env_temperature = dht.readTemperature();
+  float env_humidity = dht.readHumidity();
+  if (!isnan(env_humidity) && !isnan(env_temperature)) {
+    display.setCursor(64, 8);
+    display.print("Env");
+    display.setCursor(64, 16);
+    display.print(env_temperature, 1);
+    display.print(static_cast<char>(247));
+    display.print("C");
+    display.setCursor(64, 24);
+    display.print(round(env_humidity));
+    display.print("%");
   }
 
   display.display();
